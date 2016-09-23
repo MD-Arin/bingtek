2ca65095a803628e57f6d3d09fdd64c9f19be937
MD-Arin <graffymint@gmail.com>
2016-09-23 19:16:25 +0300

    Added the third product or service Frontend Page

---
+++ views/third-ps.php
@@ -0,0 +1,69 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="container">
+      <hr class="underline">
+      <h2 class="list-group-item-heading text-center">Heading</h2>
+      <hr class="underline">
+      <div class="jumbotron">
+        <p class="list-group-item-text text-center">
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus. Commodo ullamcorper pede eu etiam consequat. Pede arcu neque mauris, sed aenean donec tempor conubia magnis amet. Posuere nunc magna enim egestas, nec nisl, libero at rutrum egestas ipsum amet, risus leo iaculis commodo. Wisi risus, posuere suspendisse tellus. Sodales diam in cursus fringilla donec, justo rutrum congue rutrum vitae lectus, consectetuer tristique, praesent est donec augue, eleifend lorem cursus sem sodales netus.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <hr class="underline">
+    <h2 class="text-center">Heading</h2>
+    <hr class="underline">
+  </div>
+
+  <div class="row vertical-line-left vertical-line-right">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <h3>Product or Service</h3>
+        <p>
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+        </p>
+        <img src="res/images/placeholder-0.jpg" class="img-thumbnail" />
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <img src="res/images/placeholder-1.jpg" class="img-thumbnail" />
+        <h3>Product or service</h3>
+        <p>
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+        </p>
+    </div>
+  </div>
+
+  <div class="row">
+    <hr class="underline">
+    <h3 class="text-center">Heading</h3>
+    <hr class="underline">
+  </div>
+
+  <div class="row vertical-line-left vertical-line-right">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <img src="res/images/placeholder-0.jpg" class="img-thumbnail" />
+        <h3>Product or Service</h3>
+        <p>
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+        </p>
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <h3>Product or service</h3>
+        <p>
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+        </p>
+        <img src="res/images/placeholder-1.jpg" class="img-thumbnail" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="jumbotron">
+      <h3 class="text-center">Heading</h3>
+      <p class="text-center">
+        Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+      </p>
+    </div>
+  </div>
+</div>
