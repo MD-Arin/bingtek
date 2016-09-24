647495db4e10f10a8198a76088fc50200f9e49ed
MD-Arin <graffymint@gmail.com>
2016-09-25 01:48:43 +0300

    Finished the fourth product page or service

---
+++ views/fourth-ps.php
@@ -0,0 +1,74 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="well-lg">
+      <img src="res/images/placeholder-0.jpg"  width="100%" height="500px"/>
+      <div class="jumbotron text-center">
+        <p>
+          Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc. Faucibus donec tortor, massa ac, sagittis fermentum placerat at quis, in rhoncus amet ultrices elit velit morbi, nullam vel. Massa pellentesque curae augue porta eget. Turpis fusce ligula cursus semper eget, porttitor pede pretium torquent eu lacus. Donec non proin mollis malesuada morbi consectetuer, sapien et.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <img src="res/images/placeholder-1.jpg" class="img-thumbnail" />
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <h4>Pointer</h4>
+      <ul>
+        <li>Point 1</li>
+        <li>Point 2</li>
+        <li>Point 3</li>
+        <li>Point 4</li>
+        <li>Point 5</li>
+      </ul>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <h4>Heading</h4>
+      <p>
+        Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc. Faucibus donec tortor, massa ac, sagittis fermentum placerat at quis, in rhoncus amet ultrices elit velit morbi, nullam vel. Massa pellentesque curae augue porta eget. Turpis fusce ligula cursus semper eget, porttitor pede pretium torquent eu lacus. Donec non proin mollis malesuada morbi consectetuer, sapien et.
+      </p>
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <img src="res/images/placeholder-0.jpg" class="img-thumbnail" />
+    </div>
+  </div>
+  <hr>
+
+  <div class="row">
+    <div class="container">
+      <div class="jumbotron">
+        <h4 class="text-center">Heading</h4>
+        <h5 class="text-center">Sub Heading</h5>
+        <hr>
+        <p class="text-center">
+          Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc. Faucibus donec tortor, massa ac, sagittis fermentum placerat at quis, in rhoncus amet ultrices elit velit morbi, nullam vel. Massa pellentesque curae augue porta eget. Turpis fusce ligula cursus semper eget, porttitor pede pretium torquent eu lacus. Donec non proin mollis malesuada morbi consectetuer, sapien et.
+        </p>
+      </div>
+    </div>
+  </div>
+  <hr>
+
+  <div class="row">
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <img src="res/images/placeholder-1.jpg" alt="placeholder-0.jpg" class="img-thumbnail" />
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <p>
+        Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc.
+      </p>
+      <hr>
+      <p>
+        Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc.
+      </p>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <img src="res/images/placeholder-0.jpg" alt="placeholder-1.jpg" class="img-thumbnail" />
+    </div>
+  </div>
+  <hr>
+</div>
