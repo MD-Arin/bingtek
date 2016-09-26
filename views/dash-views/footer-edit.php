2706fdf366d78f4ce9b584d82cfd31332e47b955
MD-Arin <graffymint@gmail.com>
2016-09-26 20:04:50 +0300

    Added footer-edit to dash-views

---
+++ views/dash-views/footer-edit.php
@@ -0,0 +1,38 @@
+<div class="container-fluid">
+  <div class="row">
+    <hr>
+    <h3>Location: Find Us</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <form class="" action="" method="post">
+      <input class="form-control margin-top" type="text" placeholder="Street or Road">
+      <input class="form-control margin-top" type="text" placeholder="Building">
+      <input class="form-control margin-top" type="text" placeholder="Building Floor">
+      <input class="btn btn-lg btn-primary margin-top" type="submit" value="Save">
+    </form>
+  </div>
+  <div class="row">
+    <hr>
+    <h3>Map</h3>
+    <hr>
+  </div>
+  <div class="row">
+      <h3>Instructions</h3>
+      <p>
+        <ol class="text-left">
+          <li>On Your Web/Internet Browser, Navigate to maps.google.com</li>
+          <li>Find and Click on the building in which your company is located</li>
+          <li>On the top left hand side of the web page, find and click on the menu icon</li>
+          <li>Navigate to share and embed option</li>
+          <li>On the modal that appears, choose the custom option</li>
+          <li>Use <i style="color: red;">200px height</i>  and <i style="color: red;">400px width</i> </li>
+          <li>Copy and paste the highlighted content to the text area below.</li>
+        </ol>
+      </p>
+      <form class="" action="" method="post">
+        <textarea class="form-control margin-top" rows="8" width=100%></textarea>
+        <input class="btn btn-lg btn-primary margin-top" type="submit" value="Save">
+      </form>
+  </div>
+</div>
