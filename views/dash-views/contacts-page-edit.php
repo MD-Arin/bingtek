92cfa6a64e4668d2e06ba24ac0dc846e12fa0e8b
MD-Arin <graffymint@gmail.com>
2016-10-07 20:59:41 +0300

    Added the contacts-page-edit and added the editing content

---
+++ views/dash-views/contacts-page-edit.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+  <div class="row">
+    <hr>
+    <h3>Google map on the Contacts page</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <hr class="underline">
+    <h3 class="text-center">Instructions</h3>
+    <hr class="underline">
+    <p>
+      <ol class="text-left">
+        <li>On Your Web/Internet Browser, Navigate to maps.google.com</li>
+        <li>Find and Click on the building in which your company is located</li>
+        <li>On the top left hand side of the web page, find and click on the menu icon</li>
+        <li>Navigate to share and embed option</li>
+        <li>On the modal that appears, choose the custom option</li>
+        <li>Copy and paste the link to the space provided below</li>
+      </ol>
+    </p>
+  </div>
+  <div class="row">
+    <form class="" action="" method="post">
+      <textarea class="form-control margin-top" rows="8" placeholder="Paste the code here"></textarea>
+      <input class="btn btn-primary margin-top" type="submit" value="Save">
+    </form>
+  </div>
+</div>
