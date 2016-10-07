fb269cb8be8b8dbc75cf18a236761ab064de0857
MD-Arin <graffymint@gmail.com>
2016-10-07 18:31:16 +0300

    Added the third-ps-page-edit and finished it

---
+++ views/dash-views/third-ps-page-edit.php
@@ -0,0 +1,159 @@
+<div class="container-fluid">
+  <div class="row">
+    <hr>
+    <h3>Product Or Service Introduction</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <hr class="underline">
+      <h2 class="list-group-item-heading text-center">Heading</h2>
+      <hr class="underline">
+      <div class="jumbotron">
+        <p class="list-group-item-text text-center">
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus. Commodo ullamcorper pede eu etiam consequat. Pede arcu neque mauris, sed aenean donec tempor conubia magnis amet. Posuere nunc magna enim egestas, nec nisl, libero at rutrum egestas ipsum amet, risus leo iaculis commodo. Wisi risus, posuere suspendisse tellus. Sodales diam in cursus fringilla donec, justo rutrum congue rutrum vitae lectus, consectetuer tristique, praesent est donec augue, eleifend lorem cursus sem sodales netus.
+        </p>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <input class="form-control margin-top" type="text" placeholder="Heading">
+          <textarea class="form-control margin-top" rows="8" placeholder="Product Introduction Content"></textarea>
+          <input class="btn btn-primary margin-top" type="submit" value="Save">
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <hr>
+    <h3>Product Or Service Description</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="row">
+        <hr class="underline">
+        <h2 class="text-center">Heading</h2>
+        <hr class="underline">
+      </div>
+
+      <div class="row vertical-line-left vertical-line-right">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <h3>Product or Service</h3>
+            <p>
+              Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+            </p>
+            <img src="res/images/placeholder-0.jpg" class="img-thumbnail" />
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img src="res/images/placeholder-1.jpg" class="img-thumbnail" />
+            <h3>Product or service</h3>
+            <p>
+              Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+            </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <input class="form-inline btn btn-primary margin-top" type="button" value="Upload Image">
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <input class="form-control margin-top" type="number" placeholder="Image No? of 2">
+          </div>
+          <br>
+          <br>
+          <br>
+          <div class="row">
+            <input class="form-control margin-top" type="text" placeholder="Heading">
+            <input class="form-control margin-top" type="text" placeholder="Sub-Heading1">
+            <textarea class="form-control margin-top" rows="8" placeholder="Content 1"></textarea>
+            <input class="btn btn-primary margin-top" type="submit" value="Save">
+            <input class="form-control margin-top" type="text" placeholder="Sub-Heading 2">
+            <textarea class="form-control margin-top" rows="8" placeholder="Content 2"></textarea>
+            <input class="btn btn-primary margin-top" type="submit" value="Save">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <hr>
+    <h3>More on the Product or Services</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="row">
+        <hr class="underline">
+        <h3 class="text-center">Heading</h3>
+        <hr class="underline">
+      </div>
+
+      <div class="row vertical-line-left vertical-line-right">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <img src="res/images/placeholder-0.jpg" class="img-thumbnail" />
+            <h3>Product or Service</h3>
+            <p>
+              Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+            </p>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <h3>Product or service</h3>
+            <p>
+              Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+            </p>
+            <img src="res/images/placeholder-1.jpg" class="img-thumbnail" />
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <input class="form-inline btn btn-primary margin-top" type="button" value="Upload Image">
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <input class="form-control margin-top" type="number" placeholder="Image No? of 2">
+          </div>
+          <br>
+          <br>
+          <br>
+          <div class="row">
+            <input class="form-control margin-top" type="text" placeholder="Heading">
+            <input class="form-control margin-top" type="text" placeholder="Sub-Heading">
+            <textarea class="form-control margin-top" rows="8" placeholder="Content"></textarea>
+            <input class="btn btn-primary margin-top" type="submit" value="Save">
+        </form>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="row">
+    <hr>
+    <h3>Final Words on the Product or Service</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="jumbotron">
+        <h3 class="text-center">Heading</h3>
+        <p class="text-center">
+          Et sit adipiscing conubia magna rutrum, aliquam facilisi, felis dolor nibh. Magna nostra velit, at hymenaeos, scelerisque in, praesent suscipit tortor dui volutpat est cum. At laoreet nunc nullam potenti, suspendisse viverra lorem molestie duis penatibus erat, elementum amet nunc, quam lobortis et amet sit, malesuada feugiat tristique mauris vestibulum malesuada lectus. Eu orci sed enim nec luctus.
+        </p>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <input class="form-control margin-top" type="text" placeholder="Heading">
+          <textarea class="form-control margin-top" rows="8" placeholder="Content"></textarea>
+          <input class="btn btn-primary margin-top" type="submit" value="Save">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
