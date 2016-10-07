dca73f60a0b4e862547756e057bd475c3766dcb0
MD-Arin <graffymint@gmail.com>
2016-10-07 20:46:43 +0300

    Added the fourth-ps-page-edit and finished adding content to it

---
+++ views/dash-views/fourth-ps-page-edit.php
@@ -0,0 +1,166 @@
+<div class="container-fluid">
+  <div class="row">
+    <hr>
+    <h3>Product or Service Introduction</h3>
+    <hr>
+  </div>
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="well-lg">
+        <img src="res/images/placeholder-0.jpg"  width="100%" height="500px"/>
+        <div class="jumbotron text-center">
+          <p>
+            Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc. Faucibus donec tortor, massa ac, sagittis fermentum placerat at quis, in rhoncus amet ultrices elit velit morbi, nullam vel. Massa pellentesque curae augue porta eget. Turpis fusce ligula cursus semper eget, porttitor pede pretium torquent eu lacus. Donec non proin mollis malesuada morbi consectetuer, sapien et.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <input class="btn btn-primary margin-top" type="button" value="Upload Image">
+          <textarea class="form-control margin-top" rows="8" placeholder="Content"></textarea>
+          <input class="btn btn-primary margin-top" type="submit" value="Save">
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <hr>
+    <h3>Pointers on the Product or Service</h3>
+    <hr>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img src="res/images/placeholder-1.jpg" class="img-thumbnail" />
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <h4>Pointer</h4>
+          <ul>
+            <li>Point 1</li>
+            <li>Point 2</li>
+            <li>Point 3</li>
+            <li>Point 4</li>
+            <li>Point 5</li>
+          </ul>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <h4>Heading</h4>
+          <p>
+            Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc. Faucibus donec tortor, massa ac, sagittis fermentum placerat at quis, in rhoncus amet ultrices elit velit morbi, nullam vel. Massa pellentesque curae augue porta eget. Turpis fusce ligula cursus semper eget, porttitor pede pretium torquent eu lacus. Donec non proin mollis malesuada morbi consectetuer, sapien et.
+          </p>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img src="res/images/placeholder-0.jpg" class="img-thumbnail" />
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <input class="btn btn-primary margin-top form-inline" type="button" value="Upload Image">
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <input class="form-control margin-top" type="number" placeholder="Image No? of 2">
+            </div>
+          </div>
+          <div class="row">
+            <input class="form-control margin-top" type="text" placeholder="pointer 1">
+            <input class="form-control margin-top" type="text" placeholder="pointer 2">
+            <input class="form-control margin-top" type="text" placeholder="pointer 3">
+            <input class="form-control margin-top" type="text" placeholder="pointer 4">
+            <input class="form-control margin-top" type="text" placeholder="pointer 5">
+            <input class="btn btn-primary margin-top" type="submit" value="Save">
+            <input class="form-control margin-top" type="text" placeholder="Heading">
+            <textarea class="form-control margin-top" rows="8" placeholder="Body Content"></textarea>
+            <input class="btn btn-primary margin-top" type="submit" value="Save">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <hr>
+    <h3>More on the Product or Service</h3>
+    <hr>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="row">
+          <div class="jumbotron">
+            <h4 class="text-center">Heading</h4>
+            <h5 class="text-center">Sub Heading</h5>
+            <hr>
+            <p class="text-center">
+              Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc. Faucibus donec tortor, massa ac, sagittis fermentum placerat at quis, in rhoncus amet ultrices elit velit morbi, nullam vel. Massa pellentesque curae augue porta eget. Turpis fusce ligula cursus semper eget, porttitor pede pretium torquent eu lacus. Donec non proin mollis malesuada morbi consectetuer, sapien et.
+            </p>
+          </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <input class="form-control margin-top" type="text" placeholder="Heading">
+          <input class="form-control margin-top" type="text" placeholder="Sub-Heading">
+          <textarea class="form-control margin-top" rows="8" placeholder="Body Content"></textarea>
+          <input class="btn btn-primary margin-top" type="submit" value="Save">
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <hr>
+    <h3>Product or Service Tagline</h3>
+    <hr>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <img src="res/images/placeholder-1.jpg" alt="placeholder-0.jpg" class="img-thumbnail" />
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <p>
+          Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc.
+        </p>
+        <hr>
+        <p>
+          Diam turpis iaculis. Nec sit non quam et, enim adipiscing elit pulvinar integer, libero neque. Interdum neque rutrum sed elit aliquam, ut luctus mollis nisl, tempor elit, metus aliquam arcu dis eos pulvinar elit, vel non nunc.
+        </p>
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <img src="res/images/placeholder-0.jpg" alt="placeholder-1.jpg" class="img-thumbnail" />
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="row">
+        <form class="" action="" method="post">
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <input class="btn btn-primary margin-top form-inline" type="button" value="Upload Image">
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <input class="form-control margin-top" type="number" placeholder="Image No? of 2">
+          </div>
+          <br>
+          <br>
+          <br>
+          <textarea class="form-control margin-top" rows="8" placeholder="Body Content 1"></textarea>
+          <textarea class="form-control margin-top" rows="8" placeholder="Body Content 2"></textarea>
+          <input class="btn btn-primary margin-top" type="submit" value="Save">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
