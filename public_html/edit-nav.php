dbd16a3b494ebf903ff54634e7667e7e53d38cdb
MD-Arin <graffymint@gmail.com>
2016-10-26 13:46:32 +0300

    Finished nav edit dashboard updating of content

--- public_html/edit-nav.php
+++ public_html/edit-nav.php
@@ -1,6 +1,34 @@
 <?php require('../model/database.php');?>
 <?php
-  if(isset($_POST['coDetails']))
+  if (isset($_POST['saveCompany']))
+  {
+    $coName = $_POST['coName'];
+    update_content('nav_content', '1-h4-company', $coName);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['saveEmail']))
+  {
+    $coEmail = $_POST['coEmail'];
+    update_content('nav_content', '1-h4-email', $coEmail);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['savePhone1']))
+  {
+    $coPhone1 = $_POST['coPhone1'];
+    update_content('nav_content', '1-h4-telephone-1', $coPhone1);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['savePhone2']))
+  {
+    $coPhone2 = $_POST['coPhone2'];
+    update_content('nav_content', '1-h4-telephone-2', $coPhone2);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif(isset($_POST['coDetails']))
   {
     $name = $_POST['coName'];
     $email = $_POST['coEmail'];
@@ -14,7 +42,36 @@
 
     return header('location: dashboard.php?q=nav');
   }
-  elseif (isset($_POST['pssave'])) {
+  elseif (isset($_POST['savefps']))
+  {
+    $fps = $_POST['fps'];
+    update_content('nav_content', '2-ul-li-fps', $fps);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['savesps']))
+  {
+    $sps = $_POST['sps'];
+    update_content('nav_content', '2-ul-li-sps', $sps);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['savetps']))
+  {
+    $tps = $_POST['tps'];
+    update_content('nav_content', '2-ul-li-tps', $tps);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['savefrthps']))
+  {
+    $frthps = $_POST['frthps'];
+    update_content('nav_content', '2-ul-li-frthps', $frthps);
+
+    return header('location: dashboard.php?q=nav');
+  }
+  elseif (isset($_POST['pssave']))
+  {
 
     $fps = $_POST['fps'];
     $sps = $_POST['sps'];
--- views/dash-views/nav-edit.php
+++ views/dash-views/nav-edit.php
@@ -6,10 +6,15 @@
   </div>
   <div class="row">
     <form class="" action="edit-nav.php" method="post">
-      <input class="form-control margin-top" type="text" name="coName" placeholder="Company Name"></input>
+      <input class="form-control" type="text" name="coName" placeholder="Company Name"></input>
+      <input class="btn btn-primary" type="submit" name="saveCompany" value="Save">
       <input class="form-control margin-top" type="email" name="coEmail" placeholder="company@email.com"></input>
+      <input class="btn btn-primary" type="submit" name="saveEmail" value="Save">
       <input class="form-control margin-top" type="number" name="coPhone1" placeholder="Company Phone Number 1"></input>
+      <input class="btn btn-primary" type="submit" name="savePhone1" value="Save">
       <input class="form-control margin-top" type="text" name="coPhone2" placeholder="Company Phone Number 2"></input>
+      <input class="btn btn-primary" type="submit" name="savePhone2" value="Save">
+      <br>
       <input class="btn btn-lg btn-primary margin-top" name="coDetails" type="submit"  value="Save All">
     </form>
   </div>
@@ -19,11 +24,16 @@
     <hr>
   </div>
   <div class="row">
-    <form class="" action="" method="post">
+    <form class="" action="edit-nav.php" method="post">
       <input class="form-control margin-top" type="text" name="fps" placeholder="First Product or service Name">
+      <input class="btn btn-primary" type="submit" name="savefps" value="Save">
       <input class="form-control margin-top" type="text" name="sps" placeholder="Second Product or service Name">
+      <input class="btn btn-primary" type="submit" name="savesps" value="Save">
       <input class="form-control margin-top" type="text" name="tps" placeholder="Third Product or service Name">
+      <input class="btn btn-primary" type="submit" name="savetps" value="Save">
       <input class="form-control margin-top" type="text" name="frthps" placeholder="Fourth Product or service Name">
+      <input class="btn btn-primary" type="submit" name="savefrthps" value="Save">
+      <br>
       <input class="btn btn-lg btn-primary margin-top" name="pssave" type="submit"  value="Save All">
     </form>
   </div>
