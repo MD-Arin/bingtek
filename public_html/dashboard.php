78f165e1a80397a2ec40b9e5a8a5a636ba5e397d
MD-Arin <graffymint@gmail.com>
2016-11-14 02:08:03 +0300

    made modifications to the dashboard sidebar

--- public_html/dashboard.php
+++ public_html/dashboard.php
@@ -15,25 +15,32 @@
       <div class="row">
         <div class="cols-xs-2 col-sm-2 col-md-2 col-lg-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="nav nav_table"><a href="">Edit Navigation</a></li>
+            <hr>
+            <li class="nav nav_table">Edit Navigation and Contacts</li>
+            <hr>
             <li><a href="dashboard.php?q=nav">Navbar and Top Panel</a></li>
             <li><a href="dashboard.php?q=footer">Footer and Bottom Panel</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Edit Pages</a></li>
-            <li><a href="dashboard.php?q=home">Home Page</a></li>
-            <li><a href="dashboard.php?q=about">About Page</a></li>
-            <li><a href="#">Products and Services</a></li>
-              <ul class="nav nav-sidebar">
-                <li><a href="dashboard.php?q=first-ps">First Product Page</a></li>
-                <li><a href="dashboard.php?q=second-ps">Second Product Page</a></li>
-                <li><a href="dashboard.php?q=third-ps">Third Product Page</a></li>
-                <li><a href="dashboard.php?q=fourth-ps">Fourth Product Page</a></li>
-              </ul>
             <li><a href="dashboard.php?q=contacts">Contacts Page</a></li>
           </ul>
           <ul class="nav nav-sidebar">
-            <li><a href="">Themes</a></li>
+            <hr>
+            <li>Edit Pages</li>
+            <hr>
+            <li><a href="dashboard.php?q=home">Home Page</a></li>
+            <li><a href="dashboard.php?q=about">About Page</a></li>
+            <hr>
+            <li>Edit Products and Services</li>
+            <hr>
+            <li><a href="dashboard.php?q=first-ps">First Product Page</a></li>
+            <li><a href="dashboard.php?q=second-ps">Second Product Page</a></li>
+            <li><a href="dashboard.php?q=third-ps">Third Product Page</a></li>
+            <li><a href="dashboard.php?q=fourth-ps">Fourth Product Page</a></li>
+
+          </ul>
+          <ul class="nav nav-sidebar">
+            <hr>
+            <li>Themes</li>
+            <hr>
             <li><a href="dashboard.php?q=bcolors">Background and Colors</a></li>
           </ul>
         </div>
